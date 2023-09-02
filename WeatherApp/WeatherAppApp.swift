4a148e4aedac724e7517363b7e5ced4c7eb0a77f
hyeondxx@gmail.com <hyeondxx@gmail.com>
2023-09-02 11:13:03 +0900

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 정현 on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
